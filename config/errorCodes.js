6ec8b7e1674540874a8d288272139a903eaaac52
Minh Nguyen <xiuquay55@gmail.com>
2022-04-16 14:59:49 +0700

    modify code

--- config/errorCodes.js
+++ config/errorCodes.js
@@ -4,7 +4,17 @@ const errorCodes = {
   InvalidSwaggerType: {
     message: 'Swagger type one of kind OPTIONS, YAML',
     returnCode: 5000,
-    statusCode: 400,
+    statusCode: 500,
+  },
+  SwaggerOptionsIsEmpty: {
+    message: 'Swagger options not found',
+    returnCode: 5001,
+    statusCode: 500,
+  },
+  SwaggerYamlIsEmpty: {
+    message: 'Swagger yaml not found',
+    returnCode: 5002,
+    statusCode: 500,
   },
 };
 
